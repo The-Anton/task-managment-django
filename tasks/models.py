1c1eee7554f84ef051007590eff8532fa9861c9b
The-Anton <shekhar59324@gmail.com>
2022-02-17 17:14:09 +0530

    add: priority field to model

--- tasks/models.py
+++ tasks/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    priority = models.IntegerField(unique=True)
+    priority = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
